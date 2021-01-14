84c055926cee4f1cb285d42642d93aa667c189c2
Dilschat <dilschat@yandex.ru>
2021-01-14 17:12:48 +0500

    finished

---
+++ sorted_iterator.jl
@@ -0,0 +1,35 @@
+using Pipe
+
+struct SortedVectors
+    data::Vector{Vector{Int}}
+end
+
+SortedVectorsIterState = Vector{Union{Int64,Nothing}}
+
+Base.iterate(arrays::SortedVectors) =
+    iterate(arrays, SortedVectorsIterState(ones(length(arrays.data))))
+
+function Base.iterate(vectors::SortedVectors, state::SortedVectorsIterState)
+    element, data_idx = @pipe zip(state, vectors.data) |>
+          map(_get_element_or_missing, _) |>
+          skipmissing(_) |>
+          _findmin(_)
+    _get_next(element, data_idx, state)
+end
+
+_findmin(itr) = isempty(itr) ? (nothing, nothing) : findmin(itr)
+
+_get_element_or_missing((idx, arr)::Tuple{Int,Int})::Union{Int,Missing} =
+    get(arr, idx, missing)
+
+_get_next(element::Int, data_idx::Int, state::SortedVectorsIterState) =
+    (data_idx, state[data_idx], element, _get_next_state(data_idx, state))
+
+_get_next(element::Nothing, data_idx::Nothing, state::SortedVectorsIterState) =
+    nothing
+
+function _get_next_state(data_idx::Int, state::SortedVectorsIterState)
+    new_state = copy(state)
+    new_state[data_idx] += 1
+    return new_state
+end
--- test_sorted_iterator.jl
+++ test_sorted_iterator.jl
@@ -1,3 +1,28 @@
 using Test
+using BenchmarkTools
 
 include("sorted_iterator.jl")
+
+function sort_test(iter::SortedVectors)
+    result = []
+    next = iterate(iter)
+    while next !== nothing
+        append!(result, next[3])
+        next = iterate(iter, next[4])
+    end
+    return result
+end
+
+
+@testset "sorted iterator of sorted data" begin
+    @test sort_test(SortedVectors([[1,2,3],[3,4]])) == [1,2,3,3,4]
+    @test sort_test(SortedVectors([[1,2,3,5],[3,4],[4,10]])) == [1, 2, 3, 3, 4, 4, 5, 10]
+    @test sort_test(SortedVectors([[1,2,3,5],[0,3,4],[4,10]])) == [0, 1, 2, 3, 3, 4, 4, 5, 10]
+    @test sort_test(SortedVectors([[1,1,1],[1,1]])) == [1,1,1,1,1]
+    @btime iterate(SortedVectors([[1],[0]]), Vector{Union{Int64, Nothing}}([1,1]))
+end
+
+@testset "sorted iterator of empty data" begin
+    @test isnothing(iterate(SortedVectors([[],[]])))
+    @test isnothing(iterate(SortedVectors([])))
+end
